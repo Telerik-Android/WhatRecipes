1fb4221c4666068e55fdf1e5f5efd0aae7b0fb56
TonyNikolov <fatalclaw@hotmail.com>
2017-02-12 20:37:29 +0200

    drawer improvements, more improvments left to be implemented

--- WhatRecipes/app/src/main/java/com/whatrecipes/whatrecipes/view/DrawerFragment.java
+++ WhatRecipes/app/src/main/java/com/whatrecipes/whatrecipes/view/DrawerFragment.java
@@ -1,6 +1,7 @@
 package com.whatrecipes.whatrecipes.view;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,43 +9,74 @@ import android.support.v7.widget.Toolbar;
 import android.view.*;
 import android.view.View;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.whatrecipes.whatrecipes.R;
 
 public class DrawerFragment extends Fragment {
+    AccountHeader accountHeader;
+    Drawable profile;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
          View view = inflater.inflate(R.layout.fragment_drawer, container, false);
-
+        Integer profile =  R.drawable.user_not_registered;
+        setupHeader(profile,"Telerik Academy","Telerik@Telerik.com");
         setupDrawer(view);
 
         return view;
     }
 
+    private void setupHeader(Integer profileImage, String accountName, String emailAddress){
+         this.accountHeader = new AccountHeaderBuilder()
+                .withActivity(this.getActivity())
+                .withHeaderBackground(R.drawable.drawer_header_nutrional_dark)
+                .addProfiles(
+                        new ProfileDrawerItem().withName(accountName).withEmail(emailAddress).withIcon(profileImage)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+    }
+
     private void setupDrawer(View view) {
+
+
         //if you want to update the items at a later time it is recommended to keep it in a variable
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_discover_recipes);
         SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_add_recipe);
+        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(3).withName(R.string.drawer_discover_recipes);
+
 
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
         toolbar.setBackgroundColor(Color.BLACK);
         toolbar.getBackground().setAlpha(90);
 
 
+
+
         //create the drawer and remember the `Drawer` result object
         Drawer result = new DrawerBuilder()
                 .withActivity(this.getActivity())
                 .withToolbar(toolbar)
+                .withAccountHeader(this.accountHeader)
                 .addDrawerItems(
-                        item1,
                         new DividerDrawerItem(),
-                        item2
+                        item2,
+                        item3
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
